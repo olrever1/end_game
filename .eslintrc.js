861903ad3b88f9ffc17039d417d4673ce5a5f4f9
olrever1 <olrever1@gmail.com>
2022-09-30 17:21:10 +0900

    first

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	rules: {
+		'no-console': 'off',
+		// "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		// "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: false,
+				useTabs: true,
+				tabWidth: 2,
+			},
+		],
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
+}
